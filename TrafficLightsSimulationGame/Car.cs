b2522153aff2c99167eb45afbb115ac3a80e1cb5
AVelickovski <a.velickovski@hotmail.com>
2015-04-28 15:26:56 +0200

    add class car

---
+++ TrafficLightsSimulationGame/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficSimulationGame
+{
+    public class Car
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        bool isWaiting { get; }
+        int velocity;
+        Image car;
+        public enum Direction
+        {
+            EAST,
+            WEST,
+            SOUTH,
+            NORTH
+        }
+        Direction dir;
+
+        public Car(int x, int y, Image car,Direction direction)
+        {
+            X = x;
+            Y = y;
+            isWaiting = false;
+            this.car = car;
+            velocity = 5;
+            dir = direction;
+        }
+
+        public void draw(Graphics g)
+        {
+            g.DrawImage(car, X, Y);
+        }
+
+        public void moveCar()
+        {
+            switch (dir)
+            {
+                case Direction.NORTH:
+                    if (Y + velocity >= 200)
+                        velocity = 0;
+                    else
+                        Y += velocity;
+                    break;
+                case Direction.SOUTH:
+                    break;
+                case Direction.EAST:
+                    break;
+                case Direction.WEST:
+                    break;
+            }
+        }
+    }
+}
